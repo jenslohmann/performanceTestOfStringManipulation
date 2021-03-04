e265e6b48d8ec58c5318ee72212eabe7695c7895
Jens Lohmann <jlo@nine.dk>
2021-03-04 11:13:55 +0100

    Added CompileStatic version.

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 One-off timings, your mileage may vary, "-server -Xmx2g -Xms2g":
 
-method | iterations | groovy | java
---- | ---:| ---:| ---:
-stringBufferExample | 100000 | 495ms | n/a
-stringBuffer2Example | 100000 | 63ms | 20ms
-stringBuilderExample | 100000 | 329ms| n/a
-stringBuilder2Example | 100000 | 30ms | 21ms
-stringExample | 100000 | 33168ms | n/a
-string2Example | 100000 | 33032ms|33386ms
-stringBufferExample | 1000000 | 2821ms | n/a
-stringBuffer2Example | 1000000 | 244ms | 57ms
-stringBuilderExample | 1000000 | 2817ms | n/a
-stringBuilder2Example | 1000000 | 243ms | 44ms
-stringBufferExample | 10000000 | 30114ms | n/a
-stringBuffer2Example | 10000000 | 2503ms | 501ms
-stringBuilderExample | 10000000 | 30679ms | n/a
-stringBuilder2Example | 10000000 | 2504ms | 497ms
-stringBuilder3Example | 10000000 | 2487ms | 554ms
+method | iterations | groovy | CompileStatic | java 8
+--- | ---:| ---:| ---:| ---:
+stringBufferExample | 100000 | 475ms | 495ms | n/a
+stringBuffer2Example | 100000 | 63ms | 66ms | 20ms
+stringBuilderExample | 100000 | 329ms| 490ms | n/a
+stringBuilder2Example | 100000 | 30ms | 39ms | 21ms
+stringExample | 100000 | 33168ms | 33473ms | n/a
+string2Example | 100000 | 33032ms | 33366ms | 33386ms
+stringBufferExample | 1000000 | 2821ms | 2869ms | n/a
+stringBuffer2Example | 1000000 | 244ms | 223ms | 57ms
+stringBuilderExample | 1000000 | 2817ms | 3326ms | n/a
+stringBuilder2Example | 1000000 | 243ms | 229ms | 44ms
+stringBufferExample | 10000000 | 30114ms | 31483ms | n/a
+stringBuffer2Example | 10000000 | 2503ms | 2307ms | 501ms
+stringBuilderExample | 10000000 | 30679ms | 35917ms | n/a
+stringBuilder2Example | 10000000 | 2504ms | 2278ms | 497ms
+stringBuilder3Example | 10000000 | 2487ms | 2326ms | 554ms
---
+++ src/dk/jlo/AppendCompileStaticPerfTest.groovy
@@ -0,0 +1,156 @@
+package dk.jlo
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class AppendCompileStaticPerfTest {
+
+    static String string1 = 'asd'
+    static String string2 = 'sdf'
+
+    static void main(String[] args) {
+        String s = ''
+        long iterations = 100_000L
+
+        time "stringBufferExample - ${iterations}", { s = stringBufferExample(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuffer2Example - ${iterations}", { s = stringBuffer2Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuilderExample - ${iterations}", { s = stringBuilderExample(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuilder2Example - ${iterations}", { s = stringBuilder2Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringExample - ${iterations}", { s = stringExample(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "string2Example - ${iterations}", { s = string2Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        iterations = 1000_000L
+
+        time "stringBufferExample - ${iterations}", { s = stringBufferExample(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuffer2Example - ${iterations}", { s = stringBuffer2Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuilderExample - ${iterations}", { s = stringBuilderExample(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuilder2Example - ${iterations}", { s = stringBuilder2Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        iterations = 10_000_000L
+
+        time "stringBufferExample - ${iterations}", { s = stringBufferExample(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuffer2Example - ${iterations}", { s = stringBuffer2Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuilderExample - ${iterations}", { s = stringBuilderExample(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuilder2Example - ${iterations}", { s = stringBuilder2Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+        System.gc()
+
+        time "stringBuilder3Example - ${iterations}", { s = stringBuilder3Example(iterations) }
+
+        assert s.length() > 1 // Værdien skal bruges, så en evt. compiler ikke optimerer den væk
+    }
+
+    static String stringBufferExample(long iterations) {
+        StringBuffer stringBuffer = new StringBuffer()
+        for (i in 0..<iterations) {
+            stringBuffer.append("${i}${string1}${string2}")
+        }
+        return stringBuffer.toString()
+    }
+
+    static String stringBuffer2Example(long iterations) {
+        StringBuffer stringBuffer = new StringBuffer()
+        for (i in 0..<iterations) {
+            stringBuffer.append(string1 + i + string2)
+        }
+        return stringBuffer.toString()
+    }
+
+    static String stringBuilderExample(long iterations) {
+        StringBuilder stringBuilder = new StringBuilder()
+        for (i in 0..<iterations) {
+            stringBuilder.append("${i}${string1}${string2}")
+        }
+        return stringBuilder.toString()
+    }
+
+    static String stringBuilder2Example(long iterations) {
+        StringBuilder stringBuilder = new StringBuilder()
+        for (i in 0..<iterations) {
+            stringBuilder.append(string1 + i + string2)
+        }
+        return stringBuilder.toString()
+    }
+
+    static String stringBuilder3Example(long iterations) {
+        StringBuilder stringBuilder = new StringBuilder(10_000_000)
+        for (i in 0..<iterations) {
+            stringBuilder.append(string1 + i + string2)
+        }
+        return stringBuilder.toString()
+    }
+
+    static String stringExample(long iterations) {
+        String string = ''
+        for (i in 0..<iterations) {
+            string += ("${i}${string1}${string2}")
+        }
+        return string
+    }
+
+    static String string2Example(long iterations) {
+        String string = ''
+        for (i in 0..<iterations) {
+            string += string1 + i + string2
+        }
+        return string
+    }
+
+    static void time(String name, Closure closure) {
+        long before = System.currentTimeMillis()
+        closure()
+        long after = System.currentTimeMillis()
+        println("${name} - ${after - before}ms elapsed.")
+    }
+}
